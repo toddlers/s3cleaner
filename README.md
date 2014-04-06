3b120f0f078eed7e3803bc4da5526870dea0e0ca
Suresh Prajapati <er.sureshprajapati@gmail.com>
2014-04-06 14:35:10 +0530

    S3cleaner script

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ s3cleaner
 =========
 
 cleaning s3 with fog
+
+λ: ruby s3cleaner.rb --help
+Usage: s3cleaner.rb [options]
+
+    -k, --key AWS_ACCESS_KEY         AWS ACCESS KEY ID
+    -s, --secret AWS_SECRET_KEY      A SECRET ACCESS KEY
+    -a, --maxage MAX_AGE             MAX_AGE In seconds
+    -r, --regex REGEX                Only consider keys matching this REGEX
+    -b, --bucket BUCKET              Search for keys in a specific bcuket
+    -d, --delete                     Actually do a delete. If not specified , just list the keys found that match
+    -h, --help                       Show this message
+
+
+λ: ruby s3cleaner.rb -k AWS_ACCESS_KEY  -s AWS_SECRET_KEY -a "1d" -b fog-demo-1396507025 -r test
+The fog-demo-1396507025 is empty !!
+
+
+λ: ruby s3cleaner.rb -k AWS_ACCESS_KEY -s AWS_SECRET_KEY -a "1d" -b fog-demo-1396507000  -r Clustering
+==Below is the list of files present in fog-demo-1396507000==
+Clustering.jar
+ClusteringNew.jar
+
+
---
+++ s3cleaner.rb
@@ -0,0 +1,145 @@
+#!/usr/bin/evn ruby
+
+# Find or delete files in S3 older than a given age and matching a pattern
+
+require 'fog'
+require 'pp'
+require 'date'
+require 'chronic'
+require 'time'
+
+class S3Cleaner
+  def self.parse(args)
+    options = {}
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: #{__FILE__} [options]"
+      opts.separator ""
+      opts.on("-k","--key AWS_ACCESS_KEY", "AWS ACCESS KEY ID") do |k|
+        options[:key] = k
+      end
+      opts.on("-s","--secret AWS_SECRET_KEY", "A SECRET ACCESS KEY") do |s|
+      options[:secret] = s
+      end
+      opts.on("-a" ,"--maxage MAX_AGE","MAX_AGE In seconds") do |a|
+        options[:maxage] = a
+      end
+      opts.on("-r","--regex REGEX","Only consider keys matching this REGEX") do |r|
+        options[:regex] = r
+      end
+      opts.on("-b","--bucket BUCKET","Search for keys in a specific bcuket") do |b|
+        options[:bucket] = b
+      end
+      options[:delete] = false
+      opts.on("-d","--delete","Actually do a delete. If not specified , just list the keys found that match") do
+        options[:delete] = true
+      end
+      opts.on_tail("-h","--help","Show this message") do
+        puts opts
+        exit
+      end
+    end
+    begin
+      opts.parse(args)
+      raise OptionParser::MissingArgument, "-k , no AWS_ACCESS_KEY specified" if not options[:key]
+      raise OptionParser::MissingArgument, "-s , no AWS_SECRET_KEY specified" if not options[:secret]
+      raise OptionParser::MissingArgument, "-a , no MAX_AGE specified" if not options[:maxage]
+      raise OptionParser::MissingArgument, "-b , no BUCKET name specified" if not options[:bucket]
+      raise OptionParser::MissingArgument, "-r , no REGEX name specified" if not options[:regex]
+    rescue SystemExit
+      exit
+    rescue Exception => e
+      #puts "Error " + e, "#{__FILE__} -h for options"
+      puts e
+      exit
+    end
+    options
+  end
+
+  # Get S3 Connection 
+
+  def self.get_s3connection(aws_access_key,aws_secret_key)
+    connection = Fog::Storage.new({
+      :provider => 'AWS',
+      :aws_access_key_id => aws_access_key,
+      :aws_secret_access_key => aws_secret_key,
+      :path_style => true
+    })
+  end
+
+  # Find files in a bucket specif to a pattern
+
+  #  <Fog::Storage::AWS::File
+  #             key="test.txt",
+  #             cache_control=nil,
+  #             content_disposition=nil,
+  #             content_encoding=nil,
+  #             content_length=28,
+  #             content_md5=nil,
+  #             content_type=nil,
+  #             etag="32b10865eeb075b3d8fdbd8918741782",
+  #             expires=nil,
+  #             last_modified=2014-04-05 06:32:57 UTC,
+  #             metadata={},
+  #             owner={:display_name=>nil, :id=>nil},
+  #             storage_class="STANDARD",
+  #             encryption=nil,
+  #             version=nil
+  #           >,
+  #
+
+  def self.list_files(connection,bucket,prefix)
+    files = {}
+    connection.directories.get(bucket, prefix: prefix).files.map do |file|
+      files[file.key] = file.last_modified
+    end
+    return files
+  end
+
+  # Get a list of files to delete
+
+  def self.files_to_delete(files,maxage)
+    fd = []
+    now = Time.now.utc
+    files.each do |name,time|
+      age = ((now - time).to_i)/(3600*24)
+      if age > maxage
+       fd << name
+      end
+    end
+    return fd
+  end
+
+  # Deleting files
+
+  def self.delete_file(connection,bucket,files)
+    connection.directories.get(bucket).files.all.each do |file|
+      files.each do |f|
+        if file.key == f
+          file.destroy
+        end
+      end
+    end
+  end
+
+  def self.run(args)
+    opts = parse(args)
+    bucket_name = opts[:bucket]
+    connection = get_s3connection(opts[:key],opts[:secret])
+    files = list_files(connection,bucket_name,opts[:regex])
+    maxage = opts[:maxage].scan(/\d+/)[0].to_i
+    if not files.empty?
+      filesToDelete =  files_to_delete(files,maxage)
+      if opts[:delete]
+        puts "==Deleting all the files in #{bucket_name} =="
+        delete_files(connection,bucket_name,filesToDelete) if not filesToDelete.empty?
+      else
+        puts "==Below is the list of files present in #{bucket_name}==\n\n"
+        puts filesToDelete.join("\n") if not filesToDelete.empty?
+      end
+    else
+      puts "The #{bucket_name} is empty !!"
+    end
+  end
+end
+
+S3Cleaner.run(ARGV)
